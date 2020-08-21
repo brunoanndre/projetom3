4b04ab13b8f559165b42f62250d7c862f24b456a
brunoanndre <69970733+brunoanndre@users.noreply.github.com>
2020-08-21 14:19:48 -0300

    Add files via upload

---
+++ salao_festas/excluir_controler.php
@@ -0,0 +1,21 @@
+<?php
+include('../Condominio/config.php');
+session_start();
+
+if($_GET['id'] == $_SESSION['Usuario']['id']){
+ $id = $_GET['id'];
+
+    $sql= "DELETE FROM reserva_salao WHERE id_condominos = " . $id;
+    
+    if($mysqli->query($sql)){
+        echo "Reserva excluída!";
+    } else{
+        echo "Erro ao excluir a reserva, contate o administrador do sistema!";
+    }
+}
+$mysqli->close();    
+
+?>
+<br>
+<button type="button" onclick="location.href='../home.php'">Voltar</button>
+
